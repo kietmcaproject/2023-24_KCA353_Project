3ad465f1a0abd55cf86ffc07b8007078eabc3484
Satyam keshar <107239908+satyamkeshar@users.noreply.github.com>
2024-02-29 10:13:37 +0530

    Add files via upload

---
+++ GA28/eduflex/public/assets/firebase.js
@@ -0,0 +1,19 @@
+import { getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// if (getApps().length === 0) {
+
+//   }
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
+
+export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
