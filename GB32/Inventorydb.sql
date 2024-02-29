8ccbb4d09ef1365e52d4231c2e6851e6fd14e8d1
Ritik0321 <151901978+Ritik0321@users.noreply.github.com>
2024-02-29 11:54:27 +0530

    Create Inventorydb.sql

---
+++ GB32/Inventorydb.sql
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href=" style.css">
+        <title>TABLE</title>
+    </head>
+    <body>
+        <table border="2" cellspacing="0" width="900" height="600" align="center">
+            <tr height="45" bgcolor="#FFFF99" >
+                <th colspan="4" >KIET</th>
+            </tr>
+            <tr height="20">
+                <th width="30">Sno</th>
+                <th>Name</th>
+                <th>Subject</th>
+                <th>Marks</th>
+            </tr>
+            <tr>
+                <th>1</th>
+                <td>Diksha Bajpai</td>
+                <td>Web Tech</td>
+                <td> 78</td>
+            </tr>
+            <tr bgcolor="ORANGE" height="50">
+                <th rowspan="2">2</th>
+                <td rowspan="2">Pooja</td>
+                <td> Opertaing System</td>
+                <td>98</td>
+                <tr bgcolor="ORANGE" height="50">
+                    <td> Mathematics</td>
+                    <td>60</td>
+                  </tr>
+            </tr>
+           
+            <tr>
+                <th>3</th>
+                <td>Monk Carle</td>
+                <td>Web Tech</td>
+                <td>78</td>
+            </tr>
+            <tr height="100" bgcolor="#ADD8E6">
+                <th>4</th>
+                <td>Albert</td>
+                <td colspan="2"> Examination pending</td>
+                
+            </tr>
+            <tr bgcolor="pink">
+                <th>5</th>
+                <td>Elelrk</td>
+                <td>Operating System</td>
+                <td rowspan="2">79</td>
+            </tr>
+           
+            
+        </table>
+
+    </body>
+</html>
