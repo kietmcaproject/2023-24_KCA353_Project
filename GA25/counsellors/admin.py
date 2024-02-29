64bc6641edfb0ae7413fdd0138075f3ee6dd09a2
SATYAM SRIVASTAVA <118372118+i-msatty@users.noreply.github.com>
2024-02-29 14:25:16 +0530

    Delete GA25/counsellors directory

--- GA25/counsellors/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from counsellors.models import counsellors
-from students.models import enrolledcourses
-
-admin.site.register(counsellors)
-admin.site.register(enrolledcourses)
--- GA25/counsellors/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CounsellorsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'counsellors'
